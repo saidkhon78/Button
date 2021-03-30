488184cdf91ea3bb83224202fae8f8f9c7f2a501
saidkhon <saidkhontursunkhujaev@gmail.com>
2021-03-30 21:29:53 +0900

    winner

--- app/src/main/java/com/androidapp/buttonapp/MyOnlcickListener.java
+++ app/src/main/java/com/androidapp/buttonapp/MyOnlcickListener.java
@@ -3,14 +3,8 @@ package com.androidapp.buttonapp;
 import android.view.View;
 
 public class MyOnlcickListener implements View.OnClickListener {
-    private final MainActivity mainActivity;
-
-    public MyOnlcickListener(MainActivity mainActivity) {
-        this.mainActivity = mainActivity;
-    }
-
     @Override
     public void onClick(View view) {
-        mainActivity.mTextView1.setText("You clicked ???의 button");
+
     }
 }
